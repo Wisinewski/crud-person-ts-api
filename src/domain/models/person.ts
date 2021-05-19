75098a2f6931f1aa5ba36164be0b547bd42abc84
Wisinewski <g_gomees@hotmail.com>
2021-05-19 15:48:56 -0300

    feat: add person model

---
+++ src/domain/models/person.ts
@@ -0,0 +1,12 @@
+export type PersonModel = {
+  id: string
+  nome: string
+  cpf: string
+  dataNascimento: string
+  paisNascimento: string
+  estadosNascimento: string
+  cidadeNascimento: string
+  email: string
+  nomePai?: string
+  nomeMae?: string
+}
