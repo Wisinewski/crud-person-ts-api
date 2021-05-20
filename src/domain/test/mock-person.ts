a6d5109648f7a51b43f68a9edf35938ce970d725
Wisinewski <g_gomees@hotmail.com>
2021-05-20 09:34:54 -0300

    test: create mock person model method

--- src/domain/test/mock-person.ts
+++ src/domain/test/mock-person.ts
@@ -1,6 +1,20 @@
+import { PersonModel } from './../models/person';
 import { AddPersonParams } from './../usecases/add-person';
 
 export const mockAddPersonParams = (): AddPersonParams => ({
+  nome: 'any_nome',
+  cpf: 'any_cpf',
+  dataNascimento: 'any_dataNascimento',
+  paisNascimento: 'any_paisNascimento',
+  estadoNascimento: 'any_estadoNascimento',
+  cidadeNascimento: 'any_cidadeNascimento',
+  email: 'any_emailNascimento',
+  nomePai: 'any_nomePai',
+  nomeMae: 'any_nomeMae'
+})
+
+export const mockPersonModel = (): PersonModel => ({
+  id: 'any_id',
   nome: 'any_nome',
   cpf: 'any_cpf',
   dataNascimento: 'any_dataNascimento',
