b18a72e36ee529fe580351422d79d75636279a5c
Wisinewski <g_gomees@hotmail.com>
2021-05-20 14:09:41 -0300

    feat: create http response forbidden helper method

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,6 +1,11 @@
 import { ServerError } from './../errors/server-error';
 import { HttpResponse } from './../protocols/http';
 
+export const forbidden = (error: Error): HttpResponse => ({
+  statusCode: 403,
+  body: error 
+})
+
 export const serverError = (error: Error): HttpResponse => ({
   statusCode: 500,
   body: new ServerError(error.stack)
