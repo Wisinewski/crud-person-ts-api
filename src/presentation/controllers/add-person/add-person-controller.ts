c27674c9e5c7f4dfc7cd2b3c3b595dc0b911e91b
Wisinewski <g_gomees@hotmail.com>
2021-05-20 14:35:59 -0300

    feat: ensure AddPersonController calls Validation with correct values

--- src/presentation/controllers/add-person/add-person-controller.ts
+++ src/presentation/controllers/add-person/add-person-controller.ts
@@ -1,3 +1,4 @@
+import { Validation } from './../../protocols/validation';
 import { CpfInUseError } from './../../errors/cpf-in-use-error';
 import { serverError, forbidden, ok } from './../../helpers/http-helper';
 import { AddPerson } from './../../../domain/usecases/add-person';
@@ -6,11 +7,13 @@ import { Controller } from './../../protocols/controller';
 
 export class AddPersonController implements Controller {
   constructor (
+    private readonly validation: Validation,
     private readonly addPerson: AddPerson
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate(httpRequest.body)
       const { nome, cpf, dataNascimento, paisNascimento, estadoNascimento, cidadeNascimento, email, nomePai, nomeMae } = httpRequest.body
       const person = await this.addPerson.add({
         nome,
