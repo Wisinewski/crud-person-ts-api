cc5a88f34daaaac13f9737633c09ff7b7dcf2311
Wisinewski <g_gomees@hotmail.com>
2021-05-20 09:37:16 -0300

    test: create LoadPersonByCpfRepositorySpy class

--- src/data/test/mock-db-person.ts
+++ src/data/test/mock-db-person.ts
@@ -1,3 +1,5 @@
+import { PersonModel } from './../../domain/models/person';
+import { LoadPersonByCpfRepository } from './../protocols/db/person/load-person-by-cpf-repository';
 import { AddPersonParams } from './../../domain/usecases/add-person';
 import { AddPersonRepository } from './../protocols/db/person/add-person-repository';
 
@@ -6,5 +8,12 @@ export class AddPersonRepositorySpy implements AddPersonRepository {
   async add (person: AddPersonParams): Promise<void> {
     this.person = person
     return null
+  }
+}
+
+export class LoadPersonByCpfRepositorySpy implements LoadPersonByCpfRepository {
+  result: PersonModel = null
+  async loadByCpf (cpf: String): Promise<PersonModel> {
+    return this.result
   }
 }
