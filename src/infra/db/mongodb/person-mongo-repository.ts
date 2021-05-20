66e2e3e994b2bfb41526141f5ccf9bfc03eca99c
Wisinewski <g_gomees@hotmail.com>
2021-05-20 11:01:06 -0300

    feat: ensure PersonMongoRepository returns a person on add success

--- src/infra/db/mongodb/person-mongo-repository.ts
+++ src/infra/db/mongodb/person-mongo-repository.ts
@@ -1,11 +1,20 @@
+import { AddPersonParams } from './../../../domain/usecases/add-person';
+import { AddPersonRepository } from './../../../data/protocols/db/add-person-repository';
 import { MongoHelper } from './helpers/mongo-helper';
 import { PersonModel } from '../../../domain/models/person';
 import { LoadPersonByCpfRepository } from '../../../data/protocols/db/load-person-by-cpf-repository';
 
-export class PersonMongoRepository implements LoadPersonByCpfRepository {
+export class PersonMongoRepository implements LoadPersonByCpfRepository, AddPersonRepository {
   async loadByCpf (cpf: String): Promise<PersonModel> {
     const personCollection = await MongoHelper.getCollection('persons')
     const person = await personCollection.findOne({ cpf })
     return person && MongoHelper.map(person)
+  }
+
+  async add (personData: AddPersonParams): Promise<PersonModel> {
+    const personCollection = await MongoHelper.getCollection('persons')
+    const result = await personCollection.insertOne(personData)
+    const person = result.ops[0]
+    return MongoHelper.map(person)
   }
 }
