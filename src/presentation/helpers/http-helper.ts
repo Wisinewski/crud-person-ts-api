d71a4bc2cb4542f4c27bcf6a49f6d59e1957c12b
Wisinewski <g_gomees@hotmail.com>
2021-05-20 14:16:53 -0300

    feat: create http response ok helper method

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,6 +1,11 @@
 import { ServerError } from './../errors/server-error';
 import { HttpResponse } from './../protocols/http';
 
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
+
 export const forbidden = (error: Error): HttpResponse => ({
   statusCode: 403,
   body: error 
