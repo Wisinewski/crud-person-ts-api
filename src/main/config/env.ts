cb520bd6dd286afc17940c71ea3a8b40f643999d
Wisinewski <g_gomees@hotmail.com>
2021-05-21 14:58:30 -0300

    chore: change mongo url

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/crud-person-ts-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/crud-person-ts-api',
   port: process.env.PORT || 5050
 }
