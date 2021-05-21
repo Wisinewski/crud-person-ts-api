f774803f80d2ec1165cb4a9e35b96b4e3a985142
Wisinewski <g_gomees@hotmail.com>
2021-05-21 09:47:41 -0300

    test: ensure EmailValidation returns an error if EmailValidator returns false

---
+++ src/validation/validators/email-validation.spec.ts
@@ -0,0 +1,25 @@
+import { EmailValidatorSpy } from './../test/mock-email-validator';
+import { InvalidParamError } from './../../presentation/errors/invalid-param-error';
+import { EmailValidation } from './email-validation';
+type SutTypes = {
+  sut: EmailValidation
+  emailValidatorSpy: EmailValidatorSpy
+}
+
+const makeSut = (): SutTypes => {
+  const emailValidatorSpy = new EmailValidatorSpy()
+  const sut = new EmailValidation('email', emailValidatorSpy)
+  return {
+    sut,
+    emailValidatorSpy
+  }
+}
+
+describe('EmailValidation', () => {
+  test('should return an error if EmailValidator returns false', async () => {
+    const { sut, emailValidatorSpy } = makeSut()
+    emailValidatorSpy.result = false
+    const error = sut.validate({ email: 'any_email@email.com' })
+    expect(error).toEqual(new InvalidParamError('email'))
+  });
+});
