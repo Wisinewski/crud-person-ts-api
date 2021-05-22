c31e267c2f46c205ffcf1c6fc43dd82ed097db02
Wisinewski <g_gomees@hotmail.com>
2021-05-22 19:27:08 -0300

    chore: add post /persons documentation

---
+++ src/main/docs/paths/person-path.ts
@@ -0,0 +1,39 @@
+export const personPath = {
+  post: {
+    tags: ['Pessoa'],
+    summary: 'API para criar uma pessoa',
+    requestBody: {
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/addPersonParams'
+          }
+        }
+      }
+    },
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/person'
+            }
+          }
+        }
+      },
+      400: {
+        $ref: '#/components/badRequest'
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  }
+}
