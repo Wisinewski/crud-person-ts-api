2782f3e132470e018e984198a24e2f9ca310a826
Wisinewski <g_gomees@hotmail.com>
2021-05-23 13:39:57 -0300

    test: ensure LoadPersonByCpfController calls Validation with correct value

---
+++ src/presentation/controllers/load-person-by-cpf/load-person-by-cpf-controller.spec.ts
@@ -0,0 +1,38 @@
+import { badRequest } from './../../helpers/http-helper';
+import { MissingParamError } from './../../errors/missing-param-error';
+import { LoadPersonByCpfSpy } from './../../test/mock-person';
+import { LoadPersonByCpfController } from './load-person-by-cpf-controller';
+import { HttpRequest } from './../../protocols/http';
+import { ValidationSpy } from '../../test/mock-validation';
+
+const mockRequest = (): HttpRequest => ({
+  params: {
+    cpf: 'any_cpf'
+  }
+})
+
+type SutTypes = {
+  sut: LoadPersonByCpfController
+  validationSpy: ValidationSpy
+  loadPersonByCpfSpy: LoadPersonByCpfSpy
+}
+
+const makeSut = (): SutTypes => {
+  const validationSpy = new ValidationSpy()
+  const loadPersonByCpfSpy = new LoadPersonByCpfSpy()
+  const sut = new LoadPersonByCpfController(validationSpy, loadPersonByCpfSpy)
+  return {
+    sut, 
+    validationSpy,
+    loadPersonByCpfSpy
+  }
+}
+
+describe('LoadPersonByCpfController', () => {
+  test('should call Validation with correct value', async () => {
+    const { sut, validationSpy } = makeSut()
+    const httpRequest = mockRequest()
+    await sut.handle(httpRequest)
+    expect(validationSpy.data).toBe(httpRequest.params)
+  });
+});
