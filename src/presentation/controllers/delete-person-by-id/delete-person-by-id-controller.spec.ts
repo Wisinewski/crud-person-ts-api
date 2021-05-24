77c8975407f1ba3590032ea689d43474479fdd4e
Wisinewski <g_gomees@hotmail.com>
2021-05-24 14:09:57 -0300

    test: ensure DeletePersonByIdController returns 400 if Validation returns an error

--- src/presentation/controllers/delete-person-by-id/delete-person-by-id-controller.spec.ts
+++ src/presentation/controllers/delete-person-by-id/delete-person-by-id-controller.spec.ts
@@ -1,3 +1,5 @@
+import { MissingParamError } from './../../errors/missing-param-error';
+import { badRequest } from './../../helpers/http-helper';
 import { DeletePersonByIdController } from './delete-person-by-id-controller';
 import { ValidationSpy } from '../../test/mock-validation';
 import { HttpRequest } from './../../protocols/http';
@@ -28,5 +30,13 @@ describe('DeletePersonByIdController', () => {
     const httpRequest = mockRequest()
     await sut.handle(httpRequest)
     expect(validationSpy.data).toEqual(httpRequest.body)
+  });
+
+  test('should return 400 if Validation returns an error', async () => {
+    const { sut, validationSpy } = makeSut()
+    const error = new MissingParamError('any_field')
+    validationSpy.result = error
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
   });
 });
