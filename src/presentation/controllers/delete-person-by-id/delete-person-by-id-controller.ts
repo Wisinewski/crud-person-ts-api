f31e6fc0d49d54f0b29d456e8b14d5535dc5b4b7
Wisinewski <g_gomees@hotmail.com>
2021-05-24 14:16:47 -0300

    feat: ensure DeletePersonByIdController calls DeletePersonById with correct value

--- src/presentation/controllers/delete-person-by-id/delete-person-by-id-controller.ts
+++ src/presentation/controllers/delete-person-by-id/delete-person-by-id-controller.ts
@@ -1,3 +1,4 @@
+import { DeletePersonById } from './../../../domain/usecases/delete-person-by-id';
 import { badRequest } from './../../helpers/http-helper';
 import { HttpRequest, HttpResponse } from './../../protocols/http';
 import { Validation } from './../../protocols/validation';
@@ -5,7 +6,8 @@ import { Controller } from './../../protocols/controller';
 
 export class DeletePersonByIdController implements Controller {
   constructor (
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly deletePersonById: DeletePersonById
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -13,6 +15,8 @@ export class DeletePersonByIdController implements Controller {
     if (error) {
       return badRequest(error)
     }
+    const { id } = httpRequest.body
+    await this.deletePersonById.delete(id)
     return null
   }
 }
