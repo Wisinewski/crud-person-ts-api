cc91dcdd53f43343ba48312acdff2436b70328c3
Wisinewski <g_gomees@hotmail.com>
2021-05-24 14:16:08 -0300

    test: create DeletePersonByIdSpy class

--- src/presentation/test/mock-person.ts
+++ src/presentation/test/mock-person.ts
@@ -1,3 +1,4 @@
+import { DeletePersonById } from './../../domain/usecases/delete-person-by-id';
 import { LoadPersonByCpf } from './../../domain/usecases/load-person-by-cpf';
 import { mockPersonModel } from './../../domain/test/mock-person';
 import { PersonModel } from './../../domain/models/person';
@@ -17,6 +18,15 @@ export class LoadPersonByCpfSpy implements LoadPersonByCpf {
   result: PersonModel = mockPersonModel()
   async load (cpf: string): Promise<PersonModel> {
     this.cpf = cpf
+    return this.result
+  }
+}
+
+export class DeletePersonByIdSpy implements DeletePersonById {
+  id: string
+  result: boolean = true
+  async delete (id: string): Promise<boolean> {
+    this.id = id
     return this.result
   }
 }
