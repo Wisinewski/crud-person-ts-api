4d298ceb024eb2888400e7e71511f109668849fe
Wisinewski <g_gomees@hotmail.com>
2021-05-25 13:46:09 -0300

    feat: ensure UpdatePersonByIdController returns 200 if UpdatePersonById returns a person

--- src/presentation/controllers/update-person-by-id/update-person-by-id-controller.ts
+++ src/presentation/controllers/update-person-by-id/update-person-by-id-controller.ts
@@ -1,6 +1,6 @@
 import { InvalidParamError } from './../../errors/invalid-param-error';
 import { UpdatePersonById } from './../../../domain/usecases/update-person-by-id';
-import { badRequest, serverError, forbidden } from './../../helpers/http-helper';
+import { badRequest, serverError, forbidden, ok } from './../../helpers/http-helper';
 import { HttpRequest, HttpResponse } from './../../protocols/http';
 import { Validation } from './../../protocols/validation';
 import { Controller } from './../../protocols/controller';
@@ -32,7 +32,7 @@ export class UpdatePersonByIdController implements Controller {
       if (!person) {
         return forbidden(new InvalidParamError('id'))
       }
-      return null
+      return ok(person)
     } catch (error) {
       return serverError(error)
     }
