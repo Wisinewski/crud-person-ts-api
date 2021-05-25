ed047416e3c276c526f59026148dc0261e202b4d
Wisinewski <g_gomees@hotmail.com>
2021-05-25 15:50:55 -0300

    test: create LoadPersonByFilterRepositorySpy class

--- src/data/test/mock-db-person.ts
+++ src/data/test/mock-db-person.ts
@@ -1,3 +1,6 @@
+import { mockPersonModel } from './../../domain/test/mock-person';
+import { FilterPersonParams } from './../../domain/usecases/load-person-by-filter';
+import { LoadPersonByFilterRepository } from './../protocols/db/load-person-by-filter-repository';
 import { UpdatePersonParams } from './../../domain/usecases/update-person-by-id';
 import { UpdatePersonByIdRepository } from './../protocols/db/update-person-by-id-repository';
 import { DeletePersonByIdRepository } from './../protocols/db/delete-person-by-id-repository';
@@ -5,7 +8,6 @@ import { PersonModel } from './../../domain/models/person';
 import { LoadPersonByCpfRepository } from './../protocols/db/load-person-by-cpf-repository';
 import { AddPersonParams } from './../../domain/usecases/add-person';
 import { AddPersonRepository } from './../protocols/db/add-person-repository';
-import { mockPersonModel } from '../../domain/test/mock-person';
 
 export class AddPersonRepositorySpy implements AddPersonRepository {
   person: AddPersonParams
@@ -39,6 +41,15 @@ export class UpdatePersonByIdRepositorySpy implements UpdatePersonByIdRepository
   result: PersonModel = mockPersonModel()
   async updateById (person: UpdatePersonParams): Promise<PersonModel> {
     this.person = person
+    return this.result
+  }
+}
+
+export class LoadPersonByFilterRepositorySpy implements LoadPersonByFilterRepository {
+  params: FilterPersonParams
+  result: PersonModel[] = [mockPersonModel(), mockPersonModel()]
+  async loadByFilter (params: FilterPersonParams): Promise<PersonModel[]> {
+    this.params = params
     return this.result
   }
 }
