7741d0cfec9b0ff0f9304fbbde493d3b1b8c5123
Wisinewski <g_gomees@hotmail.com>
2021-05-25 15:51:23 -0300

    test: create mock filter person params helper method

--- src/domain/test/mock-person.ts
+++ src/domain/test/mock-person.ts
@@ -1,3 +1,4 @@
+import { FilterPersonParams } from './../usecases/load-person-by-filter';
 import { UpdatePersonParams } from './../usecases/update-person-by-id';
 import { PersonModel } from './../models/person';
 import { AddPersonParams } from './../usecases/add-person';
@@ -37,4 +38,13 @@ export const mockUpdatePersonParams = (): UpdatePersonParams => ({
   email: 'any_emailNascimento',
   nomePai: 'any_nomePai',
   nomeMae: 'any_nomeMae'
+})
+
+export const mockFilterPersonParams = (): FilterPersonParams => ({
+  nome: 'any_nome',
+  cpf: 'any_cpf',
+  dataNascimento: 'any_dataNascimento',
+  paisNascimento: 'any_paisNascimento',
+  estadoNascimento: 'any_estadoNascimento',
+  cidadeNascimento: 'any_cidadeNascimento'
 })
