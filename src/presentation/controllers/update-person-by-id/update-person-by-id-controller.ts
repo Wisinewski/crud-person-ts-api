78c96af7e57cdf26a47692132f906731b878c4d6
Wisinewski <g_gomees@hotmail.com>
2021-05-25 13:29:25 -0300

    feat: ensure UpdatePersonByIdController calls UpdatePersonById with correct values

--- src/presentation/controllers/update-person-by-id/update-person-by-id-controller.ts
+++ src/presentation/controllers/update-person-by-id/update-person-by-id-controller.ts
@@ -1,3 +1,4 @@
+import { UpdatePersonById } from './../../../domain/usecases/update-person-by-id';
 import { badRequest } from './../../helpers/http-helper';
 import { HttpRequest, HttpResponse } from './../../protocols/http';
 import { Validation } from './../../protocols/validation';
@@ -5,14 +6,27 @@ import { Controller } from './../../protocols/controller';
 
 export class UpdatePersonByIdController implements Controller {
   constructor (
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly updatePersonById: UpdatePersonById
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = await this.validation.validate(httpRequest.body)
+    const error = this.validation.validate(httpRequest.body)
     if (error) {
       return badRequest(error)
     }
+    const { id, nome, dataNascimento, paisNascimento, estadoNascimento, cidadeNascimento, email, nomePai, nomeMae } = httpRequest.body
+    await this.updatePersonById.update({
+      id, 
+      nome, 
+      dataNascimento, 
+      paisNascimento, 
+      estadoNascimento, 
+      cidadeNascimento, 
+      email, 
+      nomePai, 
+      nomeMae
+    })
     return null
   }
 }
