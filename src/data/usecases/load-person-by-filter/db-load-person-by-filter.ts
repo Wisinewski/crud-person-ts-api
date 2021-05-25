c1ee898063f7e76bb21051b9d2c4d3706960231e
Wisinewski <g_gomees@hotmail.com>
2021-05-25 15:52:03 -0300

    feat: ensure DbLoadPersonByFilter calls LoadPersonByFilterRepository with correct values

---
+++ src/data/usecases/load-person-by-filter/db-load-person-by-filter.ts
@@ -0,0 +1,14 @@
+import { LoadPersonByFilterRepository } from './../../protocols/db/load-person-by-filter-repository';
+import { PersonModel } from './../../../domain/models/person';
+import { LoadPersonByFilter, FilterPersonParams } from './../../../domain/usecases/load-person-by-filter';
+
+export class DbLoadPersonByFilter implements LoadPersonByFilter {
+  constructor (
+    private readonly loadPersonByFilterRepository: LoadPersonByFilterRepository
+  ) {}
+
+  async load (params: FilterPersonParams): Promise<PersonModel[]> {
+    await this.loadPersonByFilterRepository.loadByFilter(params)
+    return null
+  }
+}
