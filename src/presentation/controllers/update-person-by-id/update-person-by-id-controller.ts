e4f7b24c92786501f71399fd7c526ae6511d441f
Wisinewski <g_gomees@hotmail.com>
2021-05-25 13:13:17 -0300

    feat: ensure UpdatePersonByIdController calls Validation with correct values

---
+++ src/presentation/controllers/update-person-by-id/update-person-by-id-controller.ts
@@ -0,0 +1,14 @@
+import { HttpRequest, HttpResponse } from './../../protocols/http';
+import { Validation } from './../../protocols/validation';
+import { Controller } from './../../protocols/controller';
+
+export class UpdatePersonByIdController implements Controller {
+  constructor (
+    private readonly validation: Validation
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.validation.validate(httpRequest.body)
+    return null
+  }
+}
