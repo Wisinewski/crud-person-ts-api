033956896f853b20abe1842d12d8023df19a7489
Wisinewski <g_gomees@hotmail.com>
2021-05-26 13:25:35 -0300

    refactor: change filter person obrigatory params to optional params

--- src/domain/usecases/load-person-by-filter.ts
+++ src/domain/usecases/load-person-by-filter.ts
@@ -1,6 +1,13 @@
 import { PersonModel } from './../models/person';
 
-export type FilterPersonParams = Omit<PersonModel, 'id' | 'email' | 'nomePai' | 'nomeMae'>
+export type FilterPersonParams = {
+  nome?: string
+  cpf?: string
+  dataNascimento?: string
+  paisNascimento?: string
+  estadoNascimento?: string
+  cidadeNascimento?: string
+}
 
 export interface LoadPersonByFilter {
   load: (params: FilterPersonParams) => Promise<PersonModel[]>
