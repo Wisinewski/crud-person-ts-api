d9894138da5756ebdb59d70ce3f3ea4baa1b5381
Wisinewski <g_gomees@hotmail.com>
2021-05-28 14:24:49 -0300

    test: ensure MongoIdValidation returns an error if MongoIdValidator returns false

---
+++ src/validation/validators/mongo-id-validation.spec.ts
@@ -0,0 +1,26 @@
+import { MongoIdValidatorSpy } from './../test/mock-mongo-id-validator';
+import { MongoIdValidation } from './mongo-id-validation';
+import { InvalidParamError } from './../../presentation/errors/invalid-param-error';
+
+type SutTypes = {
+  sut: MongoIdValidation
+  mongoIdValidatorSpy: MongoIdValidatorSpy
+}
+
+const makeSut = (): SutTypes => {
+  const mongoIdValidatorSpy = new MongoIdValidatorSpy()
+  const sut = new MongoIdValidation('id', mongoIdValidatorSpy)
+  return {
+    sut,
+    mongoIdValidatorSpy
+  }
+}
+
+describe('MongoIdValidation', () => {
+  test('should return an error if MongoIdValidator returns false', async () => {
+    const { sut, mongoIdValidatorSpy } = makeSut()
+    mongoIdValidatorSpy.result = false
+    const error = sut.validate({ id: 'any_id' })
+    expect(error).toEqual(new InvalidParamError('id'))
+  });
+});
