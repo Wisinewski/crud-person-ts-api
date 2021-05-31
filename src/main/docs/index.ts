6c1d26ce6c1c9f433a8cd415652a11aff848a13a
Wisinewski <g_gomees@hotmail.com>
2021-05-31 14:49:01 -0300

    chore: release 4.1.0

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -7,7 +7,7 @@ export default {
   info: {
     title: 'crud-person-ts-api',
     description: 'API para realizar CRUD de pessoas',
-    version: '4.0.3'
+    version: '4.1.0'
   },
   servers: [{
     url: '/api'
